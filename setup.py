2cc2e52e6ce8bbe833f1a329401a2603f2715d11
crousseau <charly.rousseau@icm-institute.org>
2021-02-02 14:19:34 +0100

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+requirements = [
+    'numpy',
+    'scipy',
+    'pandas',
+    'matplotlib',
+    'seaborn',
+    'scikit-learn',
+    'moviepy'
+]
+
+setup(
+    name='Fiber_Photometry_Analysis',
+    version='1.0.0',
+    install_requires=requirements,
+    packages=['fiber_photometry_analysis'],
+    python_requires='>=3.1',
+    # entry_points={
+    #     'console_scripts': [
+    #         'photometry_analysis=fiber_photometry_analysis.pipeline:main',
+    #     ],
+    # },
+    url='',
+    license='GPLv2',
+    author='Tom-top',
+    author_email='thomas.topilko@icm-institute.org',
+    description='Python toolkit for the analysis of fiber photometry data and related video recordings. '
+)
